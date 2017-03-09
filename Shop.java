1edc3f62345b68468ce80ecb6b3c81595cde6333
George Broadley <gbroadley96@gmail.com>
2017-03-09 00:35:34 +0000

    Finished Step 2

--- Shop.java
+++ Shop.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.awt.*;
+import java.io.*;
 
 /**
  * Shop to store tools
@@ -24,9 +25,59 @@ public class Shop
     {
         FileDialog fileDialog = new FileDialog(new Frame(), "Open", FileDialog.LOAD);
         fileDialog.setVisible(true);
-        String filename = fileDialog.getFile();
-
-        System.out.println(filename);
+        String filename = fileDialog.getDirectory() + fileDialog.getFile();        
+        if (filename == null) {
+            System.out.println("Error 01: File not found, please try again with a valid file");
+        }
+        else
+        {
+            try
+            {
+                File file = new File(filename);
+                Scanner scanner = new Scanner(file);
+        
+                while (scanner.hasNext())
+                {
+                    String nextLine = scanner.nextLine().trim();
+                                        
+                    if (!checkIfEmptyOrComment(nextLine))
+                    {
+                        Scanner fieldScanner = new Scanner(nextLine);
+                        fieldScanner.useDelimiter(",");
+                        boolean rechargeable = fieldScanner.nextBoolean();
+                        String power = fieldScanner.next();
+                        String toolName = fieldScanner.next();
+                        String itemCode = fieldScanner.next();
+                        int timesBorrowed = fieldScanner.nextInt();
+                        boolean onLoan = fieldScanner.nextBoolean();
+                        int cost = fieldScanner.nextInt();
+                        int weight = fieldScanner.nextInt();
+                        
+                        toolsList.add(new ElectricTool(toolName, itemCode, timesBorrowed, onLoan, cost,weight, rechargeable, power));
+                    }
+                }
+            }
+            catch(FileNotFoundException e)
+            {
+                System.out.println("Error 02: File not found, please try again with a valid file");
+            }
+        }
+    }
+    
+    private boolean checkIfEmptyOrComment(String lineToCheck)
+    {
+        if (lineToCheck.equals(""))
+        {
+            return true;
+        }
+        else if (lineToCheck.substring(0,2).equals("//"))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     public void printAllDetails()
@@ -46,7 +97,7 @@ public class Shop
         return toolsList;
     }
 
-    public void setToolsList(ArrayList toolsList)
+    public void setToolsList(ArrayList<Tool> toolsList)
     {
         this.toolsList = toolsList;
     }
